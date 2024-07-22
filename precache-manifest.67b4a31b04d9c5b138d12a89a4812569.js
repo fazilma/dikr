89561d01fd18ce7cd76d00c7574d9a2b1ca7ccd3
Fazil MA <maf.fazil@gmail.com>
2024-07-22 14:51:24 +0400

    Updates

---
+++ precache-manifest.67b4a31b04d9c5b138d12a89a4812569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e9342a76cd412fd9ebfe4d6ec88beb",
+    "url": "/dikr/index.html"
+  },
+  {
+    "revision": "f88422ecd1acd6934968",
+    "url": "/dikr/static/css/main.f68c9830.chunk.css"
+  },
+  {
+    "revision": "d9e3429ffee9fbce7753",
+    "url": "/dikr/static/js/2.60b28956.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/dikr/static/js/2.60b28956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88422ecd1acd6934968",
+    "url": "/dikr/static/js/main.6d4a6708.chunk.js"
+  },
+  {
+    "revision": "61c9113e82480357a380",
+    "url": "/dikr/static/js/runtime-main.b26c6695.js"
+  }
+]);
